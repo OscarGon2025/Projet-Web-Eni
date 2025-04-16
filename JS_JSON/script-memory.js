ccac97972f8f45a67fcc34c49cd66bfb907ffcb2
Oscar GONZALEZ <oscar.gonzalez2025@campus-eni.fr>
2025-04-16 15:47:12 +0200

    Feat: Modal Overlay, Formulaire S'inscrire

--- JS_JSON/script-memory.js
+++ JS_JSON/script-memory.js
@@ -7,3 +7,27 @@ function updatemenu() {
     }
   }
   
+
+ //MODAL OVERLAY TEST//
+  
+  const openModalBtn = document.getElementById("openModalBtn");
+  const closeModalBtn = document.getElementById("closeModalBtn");
+  const modal = document.getElementById("modalOverlay");
+
+  openModalBtn.addEventListener("click", (e) => {
+    e.preventDefault(); // evita el salto de página si es un <a>
+    modal.style.display = "block";
+  });
+
+  closeModalBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+
+  //
